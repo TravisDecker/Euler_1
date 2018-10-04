4b4a7cb5d0613c4439f2dc4c31ca1a8d9a91b905
Travis Decker <decker4693@gmail.com>
2018-10-04 13:27:39 -0600

    corrected the multiple addition  of operand that was devisable by 3 and 5.

--- src/edu/cnm/deepdive/Test.java
+++ src/edu/cnm/deepdive/Test.java
@@ -1,23 +1,30 @@
 package edu.cnm.deepdive;
 
-public class Euler1 {
+public class Test {
+
+  private int sum;
+
 
   public static void main(String[] args) {
-     int sum = 0;
-    for (int i = 1; i < 1000; i++) {
+
+  }
+
+  void ThreeOrFive() {
+    for (int i = 1; i <= 1000; i++) {
       String value = "";
       if (i % 3 == 0) {
         sum += i;
       }
-      if (i % 3 == 0) {
-        continue;
-      } else if (i % 5 == 0) {
+      if (i % 5 == 0) {
         sum += i;
       }
 
     }
-    System.out.println(sum);
+
   }
 
 }
+
+
+
 
